82a71ffe24aceaa0794b4b6771bd2a4fb44ab9b6
Francis Therien <frtherien@gmail.com>
2023-01-15 16:40:39 -0500

    Fix DFLL 48 Mhz clock init

--- src/clocks.nim
+++ src/clocks.nim
@@ -11,14 +11,14 @@ proc initDfll48m*() =
   ]#
 
   # Set the correct number of wait states for 48 MHz @ 3.3v */
-  # Modify a single bitfield of the CTRLB register
   NVMCTRL.CTRLB.modifyIt:
-    it.RWS = NVMCTRL_CTRLB_RWS.HALF
+    it.RWS = 1.NVMCTRL_CTRLB_RWS
 
   # Enable external 32K crystal oscillator.
-  # Write a full value to the XOSC32K register.
-  # Unspecified bitfields use the reset value as default.
-  SYSCTRL.XOSC32K.write(XTALEN=true, STARTUP=0x7, EN32K=true)
+  # Note: Explicitly set ONDEMAND=false because the argument default is true
+  SYSCTRL.XOSC32K.write(
+    XTALEN=true, STARTUP=0x7, EN32K=true, ONDEMAND=false
+  )
 
   # This has to be a separate write as per datasheet section 17.6.3
   SYSCTRL.XOSC32K.modifyIt:
@@ -29,13 +29,12 @@ proc initDfll48m*() =
   while not SYSCTRL.PCLKSR.read().XOSC32KRDY: discard
 
   # Set GCLK1 divider to 1
-  # Note: field "DIV" is renamed to "DIVx" because "div" is a reserved keyword
-  # in Nim.
   GCLK.GENDIV.write(ID=1, DIVx=1)
-  while GCLK.STATUS.read().SYNCBUSY: discard
 
   # Set up GCLK1 to use XOSC32K as source
   GCLK.GENCTRL.write(ID=1, SRC=XOSC32K, IDC=true, GENEN=true)
+
+  # Wait for the write to complete
   while(GCLK.STATUS.read().SYNCBUSY): discard
 
   # Set GCLK1 as source for DFLL
@@ -44,13 +43,15 @@ proc initDfll48m*() =
   # This works around a quirk in the hardware (errata 1.2.1) -
   # the DFLLCTRL register must be manually reset to this value before
   # configuration.
+  # Note: Explicitly set ONDEMAND=false because the argument default is true
   while not SYSCTRL.PCLKSR.read().DFLLRDY: discard
-  SYSCTRL.DFLLCTRL.write(ENABLE=true)
+  SYSCTRL.DFLLCTRL.write(ENABLE=true, ONDEMAND=false)
   while not SYSCTRL.PCLKSR.read().DFLLRDY: discard
 
   # Set up the DFLL  multiplier. This tells the DFLL to multiply the 32.768 kHz
   # reference clock to 48 MHz
   SYSCTRL.DFLLMUL.write(MUL=1465, FSTEP=511, CSTEP=31)
+  while not SYSCTRL.PCLKSR.read().DFLLRDY: discard
 
   # Read factory calibration for DFLLVAL.COARSE The fuse addresses are not
   # included in our SVD-generated device module, so the following addresses were
