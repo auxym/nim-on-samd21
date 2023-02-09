0b05ac25ce428c431e8431e64c874ee93783e2fc
Francis Therien <frtherien@gmail.com>
2023-02-08 21:49:42 -0500

    WIP

--- src/startup2.nim
+++ src/startup2.nim
@@ -8,25 +8,30 @@ let
   sfixed {.importc: "_sfixed".}: uint32
 
 
-## Get address of something as bare unsigned integer
-template mloc(x: untyped): untyped =
-  cast[uint](unsafeAddr x)
+proc Reset_Handler {.exportc.} =
+  template mloc(x: untyped): untyped =
+    ## Get address of something as bare unsigned integer
+    cast[uint](unsafeAddr x)
 
-# Initialize the relocate segment
-block:
-  var
-    src = cast[ptr UncheckedArray[uint32]](etext.unsafeAddr)
-    dest = cast[ptr UncheckedArray[uint32]](srelocate.unsafeAddr)
-    i = 0
-  while mloc(dest[i]) < mloc(erelocate):
-    dest[i] = src[i]
-    inc i
+  # Initialize the relocate segment
+  block:
+    var
+      src = cast[ptr UncheckedArray[uint32]](etext.unsafeAddr)
+      dest = cast[ptr UncheckedArray[uint32]](srelocate.unsafeAddr)
+      i = 0
+    while mloc(dest[i]) < mloc(erelocate):
+      dest[i] = src[i]
+      inc i
 
-# Clear the zero segment
-block:
-  var
-    i = 0
-    dest = cast[ptr UncheckedArray[uint32]](szero)
-  while mloc(dest[i]) < mloc(ezero):
-    dest[i] = 0
-    inc i
+  # Clear the zero segment
+  block:
+    var
+      i = 0
+      dest = cast[ptr UncheckedArray[uint32]](szero)
+    while mloc(dest[i]) < mloc(ezero):
+      dest[i] = 0
+      inc i
+
+  # Set the vector table base address
+  block:
+    let vectorsAddr = mloc sfixed
